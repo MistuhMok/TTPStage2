6c79766ef096f63722ad7da7f72c569c8e263983
Alex Mok <mokalexander00@gmail.com>
2019-10-03 15:28:08 -0400

    fix: removed product and reviews

--- db/models/index.js
+++ db/models/index.js
@@ -1,10 +1,14 @@
 const User = require('./user');
 const Portfolio = require('./portfolio');
+const Transaction = require('./transaction');
 
 // Product.hasMany(Review);
 // Review.belongsTo(Product);
+User.hasMany(Transaction);
+Portfolio.belongsTo(User);
 
 module.exports = {
   User,
   Portfolio,
+  Transaction,
 };
